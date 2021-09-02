35c68b1912b4d32bf89e3e921c175a2ad809d8a6
abubogale <abubogale342@gmail.com>
2021-09-02 20:13:51 +0300

    fetching movies

---
+++ utils.js
@@ -0,0 +1,11 @@
+debounce = (callback, delay = 1000) => {
+	let timeoutId;
+	return (...args) => {
+		if (timeoutId) {
+			clearTimeout(timeoutId);
+		}
+		timeoutId = setTimeout(() => {
+			callback.apply(null, args);
+		}, delay);
+	}
+}
